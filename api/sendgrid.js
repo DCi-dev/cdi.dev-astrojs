07f802e63b910d38cce06aa556423ebb0083dcac
DanCristianIlie <ilie.dancrist@protonmail.com>
2022-11-18 22:17:57 +0200

    fix contact form api

--- api/sendgrid.js
+++ api/sendgrid.js
@@ -2,13 +2,13 @@ import mail from '@sendgrid/mail';
 
 export default function handler(request, response) {
 	mail.setApiKey(process.env.PUBLIC_SENDGRID_API_KEY);
-	const { email, phone, message } = request.query;
+	const { email, firstname, lastname, subject, message } = request.query;
 	const msg = {
 		to: process.env.PUBLIC_RECEIVER_EMAIL,
 		from: process.env.PUBLIC_SENDER_EMAIL,
 		subject: 'New Submission from Astro',
-		text: `Email: ${email}\nPhone: ${phone}\n\n${message}`,
-		html: `Email: ${email}<br/>Phone: ${phone}<br/><br/>${message}`,
+		text: `Email: ${email}\FirstName: ${firstname}\LastName: ${lastname}\subject: ${subject}\n\n${message}`,
+		html: `Email: ${email}<br/>First Name: ${firstname}<br/>Last Name: ${lastname}<br/>Subject: ${subject}<br/><br/>${message}`,
 	};
 
 	mail
