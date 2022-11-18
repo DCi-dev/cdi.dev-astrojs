ffc17158cbce57c3753bf60097cc00cad84e210e
DanCristianIlie <ilie.dancrist@protonmail.com>
2022-11-18 22:34:32 +0200

    retry contact form

--- api/sendgrid.js
+++ api/sendgrid.js
@@ -1,23 +1,52 @@
-import mail from '@sendgrid/mail';
+import sendgrid from '@sendgrid/mail';
+import { z } from 'zod';
+import { env } from '../src/env/server.mjs';
 
-export default function handler(request, response) {
-	mail.setApiKey(process.env.PUBLIC_SENDGRID_API_KEY);
-	const { email, firstname, lastname, subject, message } = request.query;
-	const msg = {
-		to: process.env.PUBLIC_RECEIVER_EMAIL,
-		from: process.env.PUBLIC_SENDER_EMAIL,
-		subject: 'New Submission from Astro',
-		text: `Email: ${email}\FirstName: ${firstname}\LastName: ${lastname}\subject: ${subject}\n\n${message}`,
-		html: `Email: ${email}<br/>First Name: ${firstname}<br/>Last Name: ${lastname}<br/>Subject: ${subject}<br/><br/>${message}`,
-	};
+sendgrid.setApiKey(env.SENDGRID_API_KEY);
 
-	mail
-		.send(msg)
-		.then(() => {
-			console.log('Email sent');
-			response.status(200);
-		})
-		.catch((error) => {
-			console.error(error);
+const ResponseData = z.object({
+	message: z.string(),
+});
+type ResponseData = z.infer<typeof ResponseData>;
+
+async function sendEmail(req, res) {
+	try {
+		await sendgrid.send({
+			to: 'contact@cdi.dev',
+			from: 'contact@cdi.dev',
+			subject: `[Lead from website] : ${req.body.subject}`,
+			html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+      <html lang="en">
+      <head>
+        <meta charset="utf-8">
+      
+        <title>Contact Form</title>
+        <meta name="description" content="Contact Form - cdi.dev">
+        <meta name="author" content="cdi.dev">
+      <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+      
+        <link rel="stylesheet" href="css/styles.css?v=1.0">
+      
+      </head>
+      
+      <body>
+        <div class="img-container" style="display: flex;justify-content: center;align-items: center;border-radius: 5px;overflow: hidden; font-family: 'helvetica', 'ui-sans';">              
+              </div>
+              <div class="container" style="margin-left: 20px;margin-right: 20px;">
+              <h3>You've got a new mail from ${req.body.firstName} ${req.body.lastName}, their email is: ✉️${req.body.email} </h3>
+              <div style="font-size: 16px;">
+              <p>Message:</p>
+              <p>${req.body.message}</p>
+              <br>
+              </div>
+      </body>
+      </html>`,
 		});
+	} catch (err) {
+		return res.status(500).json({ message: 'Error -  contact form API' });
+	}
+
+	return res.status(200).json({ message: '' });
 }
+
+export default sendEmail;
--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,42 +1,65 @@
-import { useStore } from '@nanostores/react';
-import React from 'react';
+import { useState } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
-import { Details } from '../../store/details';
+import { z } from 'zod';
 
 export default function ContactUs() {
-	const details = useStore(Details);
+	const [firstName, setFirstName] = useState('');
+	const [lastName, setLastName] = useState('');
+	const [email, setEmail] = useState('');
+	const [subject, setSubject] = useState('');
+	const [message, setMessage] = useState('');
 
-	const handleSubmit = async (e) => {
+	const [loading, setLoading] = useState(false);
+
+	const handleSubmit = async (e: { preventDefault: () => void }) => {
 		e.preventDefault();
-		if (!details.sent) {
-			// Check if the message is sent already
-			Details.setKey('sent', true);
-			Details.setKey('loading', true);
-			const params = {
-				firstname: details.firstname,
-				lastname: details.lastname,
-				email: details.email,
-				subject: details.subject,
-				message: details.message,
-			}; // Request to serverless function with the query
-			try {
-				await fetch('/api/send?' + new URLSearchParams(params)).then((res) => {
-					console.log(res.status);
-					if (res.status === 200) {
-						Details.setKey('loading', false);
-						toast.success('Message Sent!');
-					} else {
-						Details.setKey('loading', false);
-						toast.error('Message Failed to Send!');
-					}
-				});
-			} catch (err) {
-				console.log(err);
-				Details.setKey('loading', false);
-				toast.error('Message Failed to Send!');
+		setLoading(true);
+		const isValidFormSchema = z.object({
+			firstName: z.string().min(3).max(50),
+			lastName: z.string().min(3).max(50),
+			email: z.string().email(),
+			subject: z.string().min(3).max(50),
+			message: z.string().min(3).max(500),
+		});
+
+		const isValidForm = isValidFormSchema.safeParse({
+			firstName,
+			lastName,
+			email,
+			subject,
+			message,
+		});
+
+		if (isValidForm.success) {
+			const res = await fetch('/api/sendgrid', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					firstName,
+					lastName,
+					email,
+					subject,
+					message,
+				}),
+			});
+
+			if (res.status === 200) {
+				toast.success('Message Sent ✌!');
+				setLoading(false);
+				setFirstName('');
+				setLastName('');
+				setEmail('');
+				setSubject('');
+				setMessage('');
+			} else {
+				toast.error('Something went wrong');
+				setLoading(false);
 			}
 		} else {
-			return;
+			toast.error('Please fill all the fields');
+			setLoading(false);
 		}
 	};
 
@@ -54,17 +77,18 @@ export default function ContactUs() {
 					<div className='group relative z-0 mb-6 w-full'>
 						<input
 							type='text'
-							name='firstname'
-							id='firstname'
+							name='firstName'
+							id='firstName'
 							className='peer mt-2 block w-full appearance-none border-0 border-b-2 border-neutral-300 bg-transparent py-2 px-1 text-sm text-neutral-900 focus:border-neutral-1000 focus:outline-none focus:ring-0 dark:border-neutral-600 dark:text-neutral-100 dark:focus:border-neutral-100'
 							placeholder=' '
 							required
+							value={firstName}
 							onChange={(e) => {
-								Details.setKey('firstname', e.target.value);
+								setFirstName(e.target.value);
 							}}
 						/>
 						<label
-							htmlFor='firstname'
+							htmlFor='firstName'
 							className='absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-neutral-600 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-bold peer-focus:text-neutral-1000 dark:text-neutral-400 dark:peer-focus:text-neutral-100'>
 							First name
 						</label>
@@ -72,17 +96,18 @@ export default function ContactUs() {
 					<div className='group relative z-0 mb-6 w-full'>
 						<input
 							type='text'
-							name='lastname'
-							id='lastname'
+							name='lastName'
+							id='lastName'
 							className='peer mt-2 block w-full appearance-none border-0 border-b-2 border-neutral-300 bg-transparent py-2 px-1 text-sm text-neutral-900 focus:border-neutral-1000 focus:outline-none focus:ring-0 dark:border-neutral-600 dark:text-neutral-100 dark:focus:border-neutral-100'
 							placeholder=' '
 							required
+							value={lastName}
 							onChange={(e) => {
-								Details.setKey('lastname', e.target.value);
+								setLastName(e.target.value);
 							}}
 						/>
 						<label
-							htmlFor='lastname'
+							htmlFor='lastName'
 							className='absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-neutral-600 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-bold peer-focus:text-neutral-1000 dark:text-neutral-400 dark:peer-focus:text-neutral-100'>
 							Last name
 						</label>
@@ -96,8 +121,9 @@ export default function ContactUs() {
 						className='peer mt-2 block w-full appearance-none border-0 border-b-2 border-neutral-300 bg-transparent py-2 px-1 text-sm text-neutral-900 focus:border-black focus:outline-none focus:ring-0 dark:border-neutral-600 dark:text-neutral-100 focus:dark:border-neutral-100'
 						placeholder=' '
 						required
+						value={email}
 						onChange={(e) => {
-							Details.setKey('email', e.target.value);
+							setEmail(e.target.value);
 						}}
 					/>
 					<label
@@ -114,8 +140,9 @@ export default function ContactUs() {
 						className='peer mt-2 block w-full appearance-none border-0 border-b-2 border-neutral-300 bg-transparent py-2 px-1 text-sm text-neutral-900 focus:border-neutral-1000 focus:outline-none focus:ring-0 dark:border-neutral-600 dark:text-neutral-100 dark:focus:border-neutral-100'
 						placeholder=' '
 						required
+						value={subject}
 						onChange={(e) => {
-							Details.setKey('subject', e.target.value);
+							setSubject(e.target.value);
 						}}
 					/>
 					<label
@@ -131,8 +158,9 @@ export default function ContactUs() {
 						className='peer mt-2 block min-h-[100px] w-full resize-y appearance-none border-0 border-b-2 border-neutral-300 bg-transparent py-2 px-1 text-sm text-neutral-900 focus:border-neutral-1000 focus:outline-none  focus:ring-0 dark:border-neutral-600 dark:text-neutral-100 dark:focus:border-neutral-100'
 						placeholder=' '
 						required
+						value={message}
 						onChange={(e) => {
-							Details.setKey('message', e.target.value);
+							setMessage(e.target.value);
 						}}
 					/>
 					<label
@@ -148,8 +176,12 @@ export default function ContactUs() {
 							type='submit'
 							className='relative w-full  overflow-hidden rounded-lg bg-primary-600 px-4 py-3 text-center text-sm font-bold text-neutral-100 outline-none transition duration-300 active:scale-95 '>
 							<div className='relative flex w-full items-center justify-center'>
+								<p
+									className={
+										loading ? 'mr-3 inline-flex animate-spin' : 'hidden'
+									}></p>
 								<p className='text-md  font-bold text-neutral-1000 md:text-lg'>
-									Send
+									{loading ? 'Sending...' : 'Send'}
 								</p>
 							</div>
 						</button>
